f7e8afb6776ab00d8275252ed3b6fa185f94571b
ZYJ-q <2979049853@qq.com>
2023-06-05 15:48:26 +0800

    l

--- src/main.rs
+++ src/main.rs
@@ -107,9 +107,10 @@ async fn real_time(
             );
             let name = f_config.name;
             let thre = f_config.threshold;
-            let threshold: f64 = thre.parse().unwrap();
+            
             println!("阈值的长度{}, 数值{}", thre.len(), thre);
             if thre.len() != 0  {
+                let threshold: f64 = thre.parse().unwrap();
             if let Some(data) = binance_futures_api.account(None).await {
                 let v: Value = serde_json::from_str(&data).unwrap();
                 let positions = v.as_object().unwrap().get("positions").unwrap().as_array().unwrap();
